c53e810da15f5ee1007ada00cee7308dab32c595
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-05-03 21:39:07 +0800

    daisy chain and select statement

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"math/rand"
 )
 
-func main() {
+type Message struct {
+	str string
+	wait chan bool
+}
+
+func main1() {
 	joe := boring("Joe")
 	ann := boring("ann")
 	// for i := 0; i < 5; i++ {
@@ -38,4 +43,49 @@ func fanIn(input1, input2 <-chan string) <-chan string {
 	go func() { for {c <- <- input1} }()
 	go func() { for {c <- <- input2} }()
 	return c
+}
+
+func newFanIn(input1, input2 <-chan string) <- chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case s := <- input1: c <- s
+			case s := <- input2: c <- s
+			}
+		}
+	}()
+	return c
+}
+
+func main2() {
+	c := boring("Joe")
+	timeout := time.After(5 * time.Second)
+	for {
+		select {
+		case s := <- c:
+			fmt.Println(s)
+		case <- timeout:
+			fmt.Println("You talked too much")
+			return
+		}
+	}
+}
+
+func main() {
+	const n = 100000
+	leftmost := make(chan int)
+	right := leftmost
+	left := leftmost
+	for i := 0; i < n; i++ {
+		right = make(chan int)
+		go f(left, right)
+		left = right
+	}
+	go func(c chan int) { c <- 1 }(right)
+	fmt.Println(<-leftmost)
+}
+
+func f(left, right chan int) {
+	left <- 1 + <- right
 }
